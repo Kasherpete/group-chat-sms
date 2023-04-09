8d56c723b835a3ef96d10f9e1b191910b5f0dd76
Keagan Peterson <Keagan.a.peterson@outlook.com>
2023-04-08 19:59:55 -0500

    Fourth commit

--- main.py
+++ main.py
@@ -13,12 +13,13 @@ def ask(number, question, timeout=60, default="", advanced=False):
     client.send_sms(number, question)
 
     while time.perf_counter() - timer_timeout <= timeout:
+
         time.sleep(1)
         new_messages = client.get_unread_messages()
 
         for message in new_messages:
-
             if message.number == message.number:
+
                 message.mark_as_read()
                 if advanced:
                     return message
@@ -29,14 +30,17 @@ def ask(number, question, timeout=60, default="", advanced=False):
 
     time.sleep(1)
     if default != "":
+
         client.send_sms(number, f'ERROR:TIMEOUT. User took too long to respond. Default response: {default}.')
+
     else:
+
         client.send_sms(number, "ERROR:TIMEOUT. User took too long to respond. Please use command again to retry.")
 
     return default
 
 
-def send_group_sms(content, except_=""):
+def send_group_sms(content, from_,  except_=""):
 
     with open("SERVER.json", 'r') as f:
 
@@ -50,8 +54,15 @@ def send_group_sms(content, except_=""):
             list1 = {"ChatNumbers": {}}
 
         for number in list1["ChatNumbers"].keys():
+
             if number != except_:
-                client.send_sms(number, f"{list1['ChatNumbers'][number]}: {content}")
+
+                if from_ != "sys":
+                    client.send_sms(number, f"{list1['ChatNumbers'][from_]}: {content}")
+                else:
+                    client.send_sms(number, f"System: {content}")
+
+            time.sleep(2)
 
 
 def is_in_chat(number):
@@ -102,6 +113,20 @@ def add(content, name):
         return True
 
 
+with open("SERVER.json", 'r') as f:
+
+    try:
+
+        list1 = json.loads(f.read())
+
+    except json.decoder.JSONDecodeError:
+
+        # if there is corruption in file
+        list1 = {"ChatNumbers": {}}
+
+
+send_group_sms("The server is online.", from_="sys")
+
 
 
 @client.on("message")
@@ -116,14 +141,15 @@ def handler(msg):
                 if add(str(msg.number), name):
 
                     msg.send_sms("You have been added!")
-                    send_group_sms(f"'{name}' has been added to the group!", except_=msg.number)
+                    send_group_sms(f"'{name}' has been added to the group!", from_="sys", except_=msg.number)
 
                 else:
 
                     msg.send_sms("Bruh you are already in this chat.")
 
         elif is_in_chat(msg.number):
-            send_group_sms(msg.content, except_=msg.number)
+
+            send_group_sms(msg.content, from_=msg.number, except_=msg.number)
 
 
 
